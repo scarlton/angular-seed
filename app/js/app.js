01f0386318fe98f9cbc1333fcfa46c4befb320a1
Steven Carlton <scarlton@navisite.com>
2014-04-23 17:51:59 -0400

    registration markup

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/', {templateUrl: 'partials/register.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/video', {templateUrl: 'partials/video.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/details', {templateUrl: 'partials/details.html', controller: 'MyCtrl1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
